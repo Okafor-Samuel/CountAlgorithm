4f2796ba39bfba5ecdc87433a7de66f7705f9a0f
decagon <samuel.okafor@decagon.dev>
2023-08-01 12:24:59 +0100

    First commit

---
+++ src/XandOChecker.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+
+public class XandOChecker {
+    public static boolean hasEqualXandO(String str){
+        String lowerCase = str.toLowerCase();
+        int  xCount = countOccured(lowerCase, 'x');
+        int oCount = countOccured(lowerCase, 'o');
+        return xCount==oCount;
+    }
+
+    private static int countOccured(String lowerCase, char x) {
+        int count =0;
+        for (int i =0; i<lowerCase.length(); i++){
+            if(lowerCase.charAt(i)==x) {
+                count++;
+            }
+        }return count;
+    }
+}
